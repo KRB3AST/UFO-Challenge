d031f140446df16282d696f39e563f6ea1b626fb
KRB3AST <59933252+KRB3AST@users.noreply.github.com>
2020-02-01 21:25:28 -0500

    Challenge

---
+++ UFO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+    private object other;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        SetCountText();
+        SetLivesText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText ();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 12)
+        {
+            transform.position = new Vector2(50.0f, 50.0f);
+        }
+        if (count == 24)
+        {
+            winText.text = "You win! Game created by Kyle Remy!";
+        }
+    }
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+            winText.text = "You lose!";
+        }
+    }
+}
